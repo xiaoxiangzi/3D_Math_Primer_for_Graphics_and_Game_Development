16f945c481fbdfd1b0b1617583b3b466f443c787
xiaoxiangzi <leijing.yang@gmail.com>
2019-12-31 17:49:34 +0800

    增加RotationMatrix旋转矩阵实现

---
+++ 3dmath/RotationMatrix.cpp
@@ -0,0 +1,9 @@
+//
+//  RotationMatrix.cpp
+//  3dmath
+//
+//  Created by xiaoxiangzi-imac on 2019/12/31.
+//  Copyright © 2019 xiaoxiangzi. All rights reserved.
+//
+
+#include "RotationMatrix.hpp"
---
+++ 3dmath/RotationMatrix.hpp
@@ -0,0 +1,14 @@
+//
+//  RotationMatrix.hpp
+//  3dmath
+//
+//  Created by xiaoxiangzi-imac on 2019/12/31.
+//  Copyright © 2019 xiaoxiangzi. All rights reserved.
+//
+
+#ifndef RotationMatrix_hpp
+#define RotationMatrix_hpp
+
+#include <stdio.h>
+
+#endif /* RotationMatrix_hpp */
